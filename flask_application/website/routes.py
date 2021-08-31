a5021f7695081b621223338e8ec13d03c3ace4b3
Ivan <ivan.shaw695@gmail.com>
2021-08-31 16:50:03 +0100

    Hangman Updated

---
+++ flask_application/website/routes.py
@@ -0,0 +1,74 @@
+# Import relevant modules
+from flask import request, render_template
+
+# Import the Flask webapp instance that we created in the __init__.py
+from website import app
+
+# Define our first route (the last part of the url for our website application)
+# We can define what urls should land in this function. Let's say / and /index
+# We can also define the legitimate methods for this page of GET and POST
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+# Now comes the actual function definition for processing this page
+def index():
+
+    # Url arguments can be added to the url like this ?name=Peter&age=57
+    # Get the url arguments if there are any
+    url_arguments =  request.args.to_dict(flat=False)
+
+    # if there are any url arguments, print them to the console here
+    if len(url_arguments) > 0:
+        print(f"\nThere were some url arguments and they were:\n{url_arguments}\n")
+
+    # When pages contain a form, we can access the variables in this function 
+    # if the form was submitted 
+    # Create a default form_package in case the form not submitted 
+    form_package = {}
+    # And now check to see if the form was actually submitted 
+    if request.method == "POST":
+
+        # pull the form fields into a dictionary for ease 
+        form_package = request.form.to_dict(flat=False)
+
+        # print the form fields to trhe console so we can see it was submitted 
+        print(f"\nThe form was submitted. The data is:\n{form_package}\n")
+
+    return render_template ('index.html',
+                            form_package = form_package,
+                            url_arguments = url_arguments) 
+
+@app.route("/bjj", methods=["GET", "POST"])
+# Now comes the actual function definition for processing this page
+def bjj():
+
+    # Url arguments can be added to the url like this ?name=Peter&age=57
+    # Get the url arguments if there are any
+    url_arguments =  request.args.to_dict(flat=False)
+
+    # if there are any url arguments, print them to the console here
+    if len(url_arguments) > 0:
+        print(f"\nThere were some url arguments and they were:\n{url_arguments}\n")
+
+    # When pages contain a form, we can access the variables in this function 
+    # if the form was submitted 
+    # Create a default form_package in case the form not submitted 
+    form_package = {}
+    # And now check to see if the form was actually submitted 
+    if request.method == "POST":
+
+        # pull the form fields into a dictionary for ease 
+        form_package = request.form.to_dict(flat=False)
+
+        # print the form fields to trhe console so we can see it was submitted 
+        print(f"\nThe form was submitted. The data is:\n{form_package}\n")
+
+    return render_template ('bjj.html',
+                            form_package = form_package,
+                            url_arguments = url_arguments) 
+
+# Now the definition of our second_page
+@app.route("/second_page", methods=["GET"])
+def second():
+    return "<a href=/index?name=Peter&age=57>Visit our home page....</a>"
+
+
--- hangman_01.py
+++ hangman_01.py
@@ -1,28 +1,29 @@
-"""
-PROBLEM: Does not work for words with duplicate letters
-Solution? incorporate it into the original function. recreate it each run through? 
-Maybe make a list and when a guess is correct, add it to the correct_guess_list and then populate the underscores with that too?
-Is there a way to gather more than one index from the original word in order to replace the duplicate in the underscore list?
-There is a replace() function that I could potentially use, but I can't specify which underscores to replace
-"""
 import random
 
-# importing the function that creates the underscores out of a word choice
-# from underscore_function import underscore_function
-def underscore_function(word_choice, guesses):
-    word_choice_blank = ''
+def get_word_list():
+    with open("word_list.txt") as file:  # open supplied txt file of words
+        bank = file.read()  # read it
+        word_list = bank.split("\n")  # split down into a list separating each line
+        return word_list
+
+
+def get_secret_word(
+    word_choice, guesses
+):  # creates the word needed to be uncovered
+    word_choice_blank = ""
     for letter in word_choice:
         if letter in guesses:
             word_choice_blank = word_choice_blank + letter
         else:
-            word_choice_blank = word_choice_blank + '_'
+            word_choice_blank = word_choice_blank + "*"
     return word_choice_blank
 
-def get_guess(guess_attempts):
+
+def get_guess(guess_attempts, secret_word):
     valid_guess = False
     while not valid_guess:
         guess = (
-            input("Please guess a letter or try to guess the whole word: ").strip().lower()
+            input(f"Your clue is: {secret_word}: Please enter your next guess: ").strip().lower()
         )  # prompt player for a guess
         if guess in guess_attempts:  # stops player from guessing the same thing twice
             print("You already used that, guess again!")
@@ -35,65 +36,49 @@ def get_guess(guess_attempts):
             continue
         valid_guess = True
     return guess
-def play_game():
-    word_bank = ["granny", "heckington", "turkey", "takeaway"]  # a simple word bank
 
+
+def play_game():
+    word_bank = get_word_list()
     word_choice_index = int(random.random() * len(word_bank))  # chooses a random index
     # chooses a word from the word bank with the index
     word_choice = word_bank[word_choice_index]
 
     # creates a pool of lives relative to the length of the word
-    lives = 6
+    lives = 7
     # turns the word into a list, might have been unnecessary
     word_choice_list = list(word_choice)
     guess_attempts = []  # creates a bank of the previously attempted guesses
 
     # uses function to produced the secret word in underscores
-    secret_word = underscore_function(word_choice, guess_attempts)
+    secret_word = get_secret_word(word_choice, guess_attempts)
     while secret_word != word_choice:
         if lives == 0:  # how to end the game with player runs out of lives
-            print("You ran out of lives, Game Over.")
+            print("you lose")
             break
         # tell the player their life count, show the word, and previous attempts
         print(
-            f"You have {lives} chances to guess correctly: \n{secret_word} you have attempted: {guess_attempts} "
+            f"You have {lives} chances to guess correctly. You have attempted: {guess_attempts}\n{secret_word}"
         )
-        guess = get_guess(guess_attempts)
-    # if it is from the alphabet
+        guess = get_guess(guess_attempts, secret_word)
+        # if it is from the alphabet
         if len(guess) == 1:  # if they guessed a letter
             if guess in word_choice:  # if letter guessed is in the word
                 print("Correct guess!")
-                secret_word = underscore_function(word_choice, guess_attempts)
+                secret_word = get_secret_word(word_choice, guess_attempts)
                 if secret_word == word_choice:  # checking if they completed the word
-                    print(
-                        f"Your secret word was: {word_choice} \nCongratulations! You have won the game with {lives} lives remaining."
-                    )
+                    print("congratulations you win")
             else:  # what happens if they guess incorrectly
                 print("Wrong guess, try again!")
                 lives = lives - 1
         else:
             if guess == word_choice:
-                print(
-                    f"Your secret word was: {word_choice} \nCongratulations! You have won the game with {lives} lives remaining."
-                )
+                print("congratulations you win")
                 break
             else:
                 print("Wrong guess, try again!")
                 lives = lives - 1
 
-if __name__ == '__main__':
-    play_game()
 
-# Make a list of words
-# Pick one a random
-# Create underscores correctly based on guesses and correct word
-# Ask for input (guess)
-# Evaluate the guess
-# Check if guess is a letter/whole word
-# Check if part of the secret word
-# If it is part, do not lose a life
-# If wrong, lose a life
-# If letter completes the word, player wins/game completes
-# Keep track of lives
-# Keep track of correct guesses
-# Print appropriate message at the end
+if __name__ == "__main__":
+    play_game()
