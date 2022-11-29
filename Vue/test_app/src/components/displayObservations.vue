77839dcec90c3f57ee7e605146d59da411c89096
Ivan <ivan.shaw695@gmail.com>
2022-11-29 22:39:12 +0000

    MASSIVE CHANGE

---
+++ Vue/test_app/src/components/displayObservations.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="mainDiv">
+        <h1>{{ test }}</h1>
+        <p>This is a bit shit</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "displayObservations",
+    data() {
+        return {
+            test: "test123"
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+font-family: 'Courier New'
+}
+</style>
