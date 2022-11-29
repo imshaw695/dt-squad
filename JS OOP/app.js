e4cfa2f17ebc86b604efd2c2366e1e0ae24e759b
Ivan <ivan.shaw695@gmail.com>
2022-11-29 20:23:56 +0000

    before .vue

--- JS OOP/app.js
+++ JS OOP/app.js
@@ -2,15 +2,23 @@ const app = Vue.createApp({
     data() {
         return {
             // I can pull encoded observation out of Observation.js, then loop over it using v-for
-            encodedObservation: {
-                "May 1":111111,
-                "May 2":222222,
-                "May 3":333333,
-                "May 4":444444
-            },
             encodedObservations: observations.observations
         }
     },
+    methods: {
+        setObservations() {
+            const observationsAsJson = JSON.stringify(this.encodedObservations);
+            let cookieName = "observations";
+            const expiryDate = new Date();
+            expiryDate.setTime(expiryDate.getTime() + (10000 * 24 * 60 * 60 * 1000));
+            let expires = "expires=" + expiryDate.toUTCString();
+            document.cookie = cookieName + "=" + observationsAsJson + ";" + expires + ";path=/";
+        },
+        deleteObservation(observationIndex) {
+            this.encodedObservations.splice(observationIndex, 1);
+            this.setObservations(this.encodedObservations);
+        }
+    }
 })
 
 app.mount("#app")
