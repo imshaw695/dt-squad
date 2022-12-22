c72ba3ecf5dbb1686f03318c9a8fd32e6f7b523b
Ivan <ivan.shaw695@gmail.com>
2022-12-22 16:19:02 +0000

    displayed users

--- Vue/vue-flask-mysql/backend/app.py
+++ Vue/vue-flask-mysql/backend/app.py
@@ -1,14 +1,14 @@
 from flask import Flask,jsonify,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
 
+CORS(app, resources={r'/*': {'origins': '*'}})
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/users'
  
-connector = mysql.connector.connect(user='root',password='root',database='flask')
-
-cursor = connector.cursor()
 
 @app.route('/', methods=['GET'])
 def get_articles():
@@ -16,26 +16,36 @@ def get_articles():
 
 @app.route('/createobservation', methods=['GET'])
 def createobservation():
-    return("Inside the flask router")
+    if request.method == "GET":
+        connector = mysql.connector.connect(user='root',password='root',database='flask')
+        cursor = connector.cursor()
+        # cursor.execute(''' INSERT INTO users VALUES("2","Bob1994", "password1234") ''')
+        # connector.commit()
+        cursor.execute(''' SELECT * from users ''')
+        data = cursor.fetchall()
+        connector.close()
+        return jsonify(data)
 
  
 @app.route('/form')
 def form():
     return render_template('form.html')
 
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if request.method == 'GET':
-        return "Login via the login Form"
+# @app.route('/login', methods = ['POST', 'GET'])
+# def login():
+#     if request.method == 'GET':
+#         return "Login via the login Form"
      
-    if request.method == 'POST':
-        id= 12345
-        name = request.form['name']
-        age = request.form['age']
-        cursor.execute(''' INSERT INTO flask_table VALUES(%s,%s,%s)''',(id,name,age))
-        connector.commit()
-        cursor.close()
-        return f"Done!!"
+#     if request.method == 'POST':
+#         connector = mysql.connector.connect(user='root',password='root',database='flask')
+#         cursor = connector.cursor()
+#         id= 12345
+#         name = request.form['name']
+#         age = request.form['age']
+#         cursor.execute(''' INSERT INTO flask_table VALUES(%s,%s,%s)''',(id,name,age))
+#         connector.commit()
+#         cursor.close()
+#         return f"Done!!"
  
 
 if __name__ == '__main__':
